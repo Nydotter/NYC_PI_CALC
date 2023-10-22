cbb60b8b59180b3d3722d82564ef5b47ab3656e5
Nydotter <christiannydegger@hotmail.com>
2023-10-22 16:02:14 +0200

    Added Leibniz Series and Buttonhandler
    Pasted in the Button handler include aswell as that C file.
    
    Started making the first simple Pi Approximation method, no outputs or anything.

--- main.c
+++ main.c
@@ -26,9 +26,18 @@
 #include "errorHandler.h"
 #include "NHD0420Driver.h"
 
+#include "ButtonHandler.h"
+
+
+
+volatile float LeibnizPi = 0;
+
+
+
 
 extern void vApplicationIdleHook( void );
-void vLedBlink(void *pvParameters);
+void vPiLeibniz(void *pvParameters);
+void vButtonTask(void *pvParameters);
 
 TaskHandle_t ledTask;
 
@@ -39,36 +48,40 @@ void vApplicationIdleHook( void )
 
 int main(void)
 {
-    resetReason_t reason = getResetReason();
-
 	vInitClock();
 	vInitDisplay();
 	
-	xTaskCreate( vLedBlink, (const char *) "ledBlink", configMINIMAL_STACK_SIZE+10, NULL, 1, &ledTask);
-
-	vDisplayClear();
-	vDisplayWriteStringAtPos(0,0,"FreeRTOS 10.0.1");
-	vDisplayWriteStringAtPos(1,0,"EDUBoard 1.0");
-	vDisplayWriteStringAtPos(2,0,"Template");
-	vDisplayWriteStringAtPos(3,0,"ResetReason: %d", reason);
+	xTaskCreate(vButtonTask, (const char *) "btTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+	//xTaskCreate( vTimeMeasurement, (const char *) "TimeMeasurement", configMINIMAL_STACK_SIZE+10, NULL, 1, &TimeMeasurement);
+	xTaskCreate( vPiLeibniz, (const char *) "Leibniz", configMINIMAL_STACK_SIZE+10, NULL, 1, NULL);
+	
 	vTaskStartScheduler();
 	return 0;
 }
 
-void vLedBlink(void *pvParameters) {
-	(void) pvParameters;
-	PORTF.DIRSET = PIN0_bm; /*LED1*/
-	PORTF.OUT = 0x01;
-	for(;;) {
-// 		uint32_t stack = get_mem_unused();
-// 		uint32_t heap = xPortGetFreeHeapSize();
-// 		uint32_t taskStack = uxTaskGetStackHighWaterMark(ledTask);
-// 		vDisplayClear();
-// 		vDisplayWriteStringAtPos(0,0,"Stack: %d", stack);
-// 		vDisplayWriteStringAtPos(1,0,"Heap: %d", heap);
-// 		vDisplayWriteStringAtPos(2,0,"TaskStack: %d", taskStack);
-// 		vDisplayWriteStringAtPos(3,0,"FreeSpace: %d", stack+heap);
-		PORTF.OUTTGL = 0x01;				
-		vTaskDelay(100 / portTICK_RATE_MS);
-	}
+void vPiLeibniz(void* pvParameters)
+{
+	uint32_t CurIterations = 0;
+	float NextSign = 1.0;
+		while (1)
+		{
+			
+			LeibnizPi = LeibnizPi + (NextSign / (2 * CurIterations + 1)) * 4;
+			NextSign = -NextSign;
+			CurIterations++;
+			
+	
+		}
 }
+
+
+void vButtonTask(void *pvParameters) {
+	initButtons();
+	vTaskDelay(3000);
+	for(;;) {
+		updateButtons();
+		vTaskDelay((100/BUTTON_UPDATE_FREQUENCY_HZ)/portTICK_RATE_MS);
+	}
+
+}
+
